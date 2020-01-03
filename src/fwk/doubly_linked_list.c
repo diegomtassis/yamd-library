d6e4400ca8cbc442c56318a114f775faae89e9e7
diegomtassis <diego.mt.dev@gmail.com>
2020-01-03 23:03:26 +0100

    * skeleton for doubly linked list

---
+++ src/fwk/doubly_linked_list.c
@@ -0,0 +1,30 @@
+/*
+ * doubly_linked_list.c
+ *
+ *  Created on: Jan 3, 2020
+ *      Author: diegomtassis
+ */
+
+#include "../../inc/fwk/doubly_linked_list.h"
+
+void doubly_linked_list_init(DoublyLinkedList* list) {
+
+}
+
+void doubly_linked_list_release(DoublyLinkedList* list) {
+
+}
+
+DoublyLinkedListNode* doubly_linked_list_add(DoublyLinkedList* list, void* e) {
+
+	return 0;
+}
+
+DoublyLinkedListNode* doubly_linked_list_find(DoublyLinkedList* list, void* e) {
+
+	return 0;
+}
+
+void doubly_linked_list_remove(DoublyLinkedList* list, DoublyLinkedListNode* node) {
+
+}
