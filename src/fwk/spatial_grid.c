a7ab417c880351481431ab848bf2d567bfdf5028
diegomtassis <diego.mt.dev@gmail.com>
2020-01-08 00:12:44 +0100

    * index optimization

--- src/fwk/spatial_grid.c
+++ src/fwk/spatial_grid.c
@@ -12,6 +12,8 @@
 #include "../../inc/fwk/commons.h"
 #include "../../inc/fwk/doubly_linked_list.h"
 
+static bool indexedInPosition(u8 dim_x, u8 dim_y, u8 x, u8 y, bool cell_indexed[dim_x][dim_y]);
+
 void spatialGridInit(SpatialGrid* grid, u8 dim_x, u8 dim_y) {
 
 	grid->dimension.x = dim_x;
@@ -69,15 +71,50 @@ void spatialGridIndex(SpatialGrid* grid, const Box_s16* object) {
 		return;
 	}
 
+	u8 dim_x = grid->dimension.x;
+	u8 dim_y = grid->dimension.y;
+
+	bool cell_indexed[dim_x][dim_y];
+	bool already_indexed = FALSE;
+
 	if (grid->cells) {
-		for (int x = 0; x < grid->dimension.x; x++) {
-			for (int y = 0; y < grid->dimension.y; y++) {
+		for (int x = 0; x < dim_x; x++) {
+			for (int y = 0; y < dim_y; y++) {
 
 				SpatialGridCell cell = grid->cells[x][y];
+
+				// in some scenarios there makes no sense to keep exploring
+				if (already_indexed) {
+
+					u8 combined_adjacents = indexedInPosition(dim_x, dim_y, x - 1, y - 1, cell_indexed)
+							+ indexedInPosition(dim_x, dim_y, x, y - 1, cell_indexed)
+							+ indexedInPosition(dim_x, dim_y, x - 1, y, cell_indexed);
+					if (combined_adjacents == 3) {
+						doublyLinkedListAdd(&grid->cells[x][y].e, object); // somehow using &cell.e passes a wrong address for the list
+						already_indexed = TRUE;
+						cell_indexed[x][y] = TRUE;
+						continue;
+
+					} else if (combined_adjacents == 2 || indexedInPosition(dim_x, dim_y, x - 1, y - 1, cell_indexed)) {
+						return;
+					}
+				}
+
 				if (overlap(&cell.aabb, object)) {
 					doublyLinkedListAdd(&grid->cells[x][y].e, object); // somehow using &cell.e passes a wrong address for the list
+					already_indexed = TRUE;
+					cell_indexed[x][y] = TRUE;
 				}
 			}
 		}
 	}
+}
+
+static bool indexedInPosition(u8 dim_x, u8 dim_y, u8 x, u8 y, bool cell_indexed[dim_x][dim_y]) {
+
+	if (x < 0 || y < 0 || x >= dim_x || y >= dim_y) {
+		return FALSE;
+	}
+
+	return cell_indexed[x][y];
 }
